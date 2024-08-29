c0e00c9daef37b265cf71ce43ac1694bdc626a67
musin2 <muhsindaud75@gmail.com>
2024-08-29 11:44:42 +0300

    adds lab solution

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []
+    if length > 0:
+         fib.append(0)
+    if length > 1:
+        fib.append(1)
+    if length >=2:
+     for n in range(2,length):
+        # i = fib.index(n)
+        i = fib[n-1] + fib[n-2]
+        fib.append(i)
+
+    print(fib)
+
+print_fibonacci(9)
